4ac5fe4f151436dd81afcd0c8a789298ee33543c
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2023-12-13 11:09:13 -0300

    Created Damain projects, Class Library and Test

---
+++ OnlineUdemyCourse/src/OnlineUdemyCourse.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineUdemyCourse.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ OnlineUdemyCourse/test/OnlineUdemyCourse.DomainTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace OnlineUdemyCourse.DomainTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
